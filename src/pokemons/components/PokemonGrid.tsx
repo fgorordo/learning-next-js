715fc762b8d5bf01ba0b0301c86fce038813e579
fgorordo <flgorordo@gmail.com>
2023-11-20 14:20:07 -0300

    ended section 6

--- src/pokemons/components/PokemonGrid.tsx
+++ src/pokemons/components/PokemonGrid.tsx
@@ -1,18 +1,21 @@
-import { PokemonCard, _SimplePokemon } from ".."
+import { SimplePokemon } from "../interfaces/simple-pokemon"
+import { PokemonCard } from "./PokemonCard";
 
 interface Props {
-    pokemons: _SimplePokemon[];
+  pokemons: SimplePokemon[];
 }
 
-export const PokemonGrid = ({ pokemons } : Props) => {
-    return (
-        <div className="flex flex-wrap gap-10 items-center justify-center">
-            {
-                pokemons.map(pokemon => (
-                    // <span key={pokemon.id}>Poke</span>
-                    <PokemonCard key={pokemon.id} pokemon={pokemon}/>
-                ))
-            }
-        </div>
-    )
+
+export const PokemonGrid = ({ pokemons }: Props) => {
+  return (
+    <div className="flex flex-wrap gap-10 items-center justify-center">
+
+        {
+          pokemons.map( pokemon => (
+            <PokemonCard key={ pokemon.id } pokemon={pokemon} />            
+          ))
+        }
+        
+    </div>
+  )
 }
--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,5 +1,8 @@
-export type { _PokemonsResponse, _Result } from "./interfaces/pokemons-response";
-export type { _SimplePokemon } from "./interfaces/simple-pokemon";
-export type { Pokemon } from "./interfaces/pokemon";
-export { PokemonGrid } from "./components/PokemonGrid";
-export { PokemonCard } from "./components/PokemonCard";
+
+
+export { PokemonGrid } from './components/PokemonGrid';
+
+
+export type { PokemonsReponse}  from './interfaces/pokemons-response';
+export type { SimplePokemon }  from './interfaces/simple-pokemon';
+export type { Pokemon }  from './interfaces/pokemon';
